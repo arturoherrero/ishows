9d04f478b515873ff92fbd1220899150719624de
Arturo Herrero <arturo.herrero@gmail.com>
2015-02-20 12:21:38 +0000

    Crop without offset

--- lib/server.rb
+++ lib/server.rb
@@ -59,7 +59,7 @@ class Server < Sinatra::Base
   end
 
   def offset(image, dimensions)
-    "+0+#{(image[:height] - dimensions.split("x").last.to_i) / 4}"
+    "+0+0"
   end
 
   def write(image, path)
