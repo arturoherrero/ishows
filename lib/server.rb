1a0562d123d2984e914354e9eb8c59896fcda171
Arturo Herrero <arturo.herrero@gmail.com>
2022-04-01 18:32:00 +0100

    Useless assignment to error variable

--- lib/server.rb
+++ lib/server.rb
@@ -35,7 +35,7 @@ class Server < Sinatra::Base
 
       send_file(filename, type: "image/jpeg", disposition: "inline")
     end
-  rescue StandardError => e
+  rescue StandardError
     Logger.new(BAD_URLS_FILE).error(url)
   end
 
